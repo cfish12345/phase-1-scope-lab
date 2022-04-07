90ad176f3f109692f0942809a88a610d6589ea83
christian fish <cfishmusic@hotmail.com>
2022-04-07 15:48:56 -0500

    commit tree b1bfa03b0475503154f9108e0f8af64bdc9ab4de

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    return window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer.overwriteBestCustomer;
+}
+
+const leastFavoriteCustomer = "jim";
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = "alex";
+}
